43cd4242b964ee18a19f341ded10029453636507
Robert Peszek <peszeks@gmail.com>
2019-04-14 19:20:41 -0600

    Performance

--- src/Present/ProofsNatAlg.hs
+++ src/Present/ProofsNatAlg.hs
@@ -7,21 +7,37 @@
   , PolyKinds
 #-}
 
-module Present.ProofsNatAlg where
+module Present.ProofsNatAlg (
+  plusCommutative
+) where
 
 import Data.Nat
 import Data.Type.Equality ((:~:)(Refl), sym)
+import Unsafe.Coerce
+
+
+-- | Using rewrite rule eliminates run time cost of proofs
+plusCommutative :: SNat left -> SNat right -> ((left + right) :~: (right + left))
+{-# NOINLINE [1] plusCommutative #-}
+plusCommutative left right = case left of 
+            SZ  -> lemma1 right
+            -- | recursive
+            (SS k)  -> case plusCommutative k right of Refl -> sym (lemma2 right k)
+
+{-# RULES "proof" forall l r. plusCommutative l r = unsafeCoerce Refl #-} 
+
+
 
 lemma1 :: SNat mx -> 'Z + mx :~: mx + 'Z
 lemma1 SZ     = Refl
+-- | recursive
 lemma1 (SS k) = case lemma1 k of  
                         Refl -> Refl
 
 lemma2 :: SNat left -> SNat right -> (left + 'S right) :~: 'S (left + right)
 lemma2 SZ right        = Refl
+-- | recursive
 lemma2 (SS left) right = case lemma2 left right of 
                         Refl -> Refl
 
-plusCommutative :: SNat left -> SNat right -> ((left + right) :~: (right + left))
-plusCommutative SZ right = lemma1 right
-plusCommutative (SS k) right = case plusCommutative k right of Refl -> sym (lemma2 right k)
+
